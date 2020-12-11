1d903296d34d7e391c8e0f91500b9ea50ddcc34b
szhaofelicia <shuyazhao2016@gmail.com>
2020-12-11 16:55:28 -0500

    Adjust parameter in packet.py

--- example.py
+++ example.py
@@ -1,30 +1,19 @@
 from os1 import OS1
 from os1.utils import xyz_points
-# from os1.packet import (
-#     AZIMUTH_BLOCK_COUNT,
-#     CHANNEL_BLOCK_COUNT,
-#     azimuth_angle,
-#     azimuth_block,
-#     azimuth_measurement_id,
-#     azimuth_timestamp,
-#     azimuth_valid,
-#     channel_block,
-#     channel_range,
-#     unpack,
-# )
 
-def handler():
+def handler(raw_packet):
     """Takes each packet and log it to a file as xyz points"""
-    print("handler!")
-    # with open('points.csv', 'a') as f:
-    #     x, y, z = xyz_points(raw_packet)
-    #     print(x,y,z)
-    #     for coords in zip(x, y, z):
-    #         f.write("{}\n".format(','.join(coords)))
+    # print("handler!")
+    with open('points.csv', 'a') as f:
+        x, y, z = xyz_points(raw_packet)
+        for coords in zip(x, y, z):
+            csv_coords = ','.join([str(c) for c in coords])
+            f.write("{}\n".format(csv_coords))
         
-os1 = OS1('172.24.113.151', '10.27.64.222', mode='1024x20') # PC_ethernet: 192.168.0.141, PC VPN:10.27.94.178, lab: 216.165.113.240
+        
+os1 = OS1('172.24.113.151', '216.165.113.240', mode='1024x20') # PC_ethernet: 192.168.0.141, PC VPN:10.27.94.178, lab: 216.165.113.240
 os1.start()
-os1.run_forever(handler,threaded=True)
+os1.run_forever(handler)
 
 
 """
