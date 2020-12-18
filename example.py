7ff587674417fedb6e3d5ec995f76961f5a0de42
szhaofelicia <shuyazhao2016@gmail.com>
2020-12-18 12:50:47 -0500

    Update example.py

--- example.py
+++ example.py
@@ -4,14 +4,15 @@ from os1.utils import xyz_points
 def handler(raw_packet):
     """Takes each packet and log it to a file as xyz points"""
     # print("handler!")
-    with open('points.csv', 'a') as f:
+    # with open('points.csv', 'a') as f:
+    with open('points_test.csv', 'w') as f:
         x, y, z = xyz_points(raw_packet)
         for coords in zip(x, y, z):
             csv_coords = ','.join([str(c) for c in coords])
             f.write("{}\n".format(csv_coords))
         
         
-os1 = OS1('172.24.113.151', '216.165.113.240', mode='1024x20') # PC_ethernet: 192.168.0.141, PC VPN:10.27.94.178, lab: 216.165.113.240
+os1 = OS1('172.24.113.151', '216.165.113.240', mode='1024x20') # PC VPN:10.27.94.178, lab: 216.165.113.240
 os1.start()
 os1.run_forever(handler)
 
